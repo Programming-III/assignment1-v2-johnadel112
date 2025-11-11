686e896f78dd237bb93a47dbcf104e214649615f
johnadel112 <johnadel02@gmail.com>
2025-11-11 09:07:33 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -3,18 +3,36 @@
 
 #include <string>
 using namespace std;
+PRIVATE:
+string name;
+int id;
 
-//#write Person class here
-
-
-
-
-
-
-
-
-
-
-
-
+PUBLIC:
+string getname();
+void setname();
+void setID();
+int getID();
+void Person();
+void displayPerson();
 #endif
+class Person{
+void Person(){
+  this-> name= name;
+  this-> id= id;}
+
+string getname(){
+return name;
+}
+int getID(){
+  return id;}
+void setname(){
+  this-> name= name;
+}
+void setID(){
+this->id=id;
+}
+void displayPerson(){
+cout<< "Name:"<< name;
+cout << "ID:"<< id;
+}
+}
