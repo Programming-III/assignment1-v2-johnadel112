05c3f67edb25e524afe99dc4c4c76bc6dda22d84
johnadel112 <johnadel02@gmail.com>
2025-11-11 09:14:43 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,21 +1,29 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
 #include "Person.h"
 #include <string>
 #using namespace std;
-//#write student class here
+PRIVATE:
+int yearLevel;
+string major;
 
-
-
-
-
-
-
-
-
-
-
-
-
+PUBLIC:
+void Student();
+void Display();
 #endif
+class Student: public Person{
+void Student(){
+this -> yearLevel= yearLevel;
+  this-> major= major;
+}
+void display(){
+cout<< "Year:" << yearLevel;
+cout << "Major:"<< major;
+  
+}
+
+
+
+
+
+}
